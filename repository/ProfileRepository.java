32b9ad379618d97a587a730103ef5db4b6790eaa
Suraj kumar <surajk3982@gmail.com>
2026-05-16 01:57:43 +0530

    Delete repository directory

--- repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobportal.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.jobportal.entity.Profile;
-
-public interface ProfileRepository extends MongoRepository<Profile, Long> {
-	
-}
